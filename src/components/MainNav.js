2d3d6c4302a32d532780858ad529d7883273bca5
NehalNagy <nehal.nagy@gmail.com>
2023-10-18 13:35:39 +0300

    padding nav items

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -2,76 +2,69 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+
 function MainNav() {
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
       <Container>
-       
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="m-auto">
-          <NavDropdown title="Shopping" id="collapsible-nav-dropdown">
+            <NavDropdown title="Shopping" id="collapsible-nav-dropdown" className="pe-2">
               <NavDropdown.Item href="#action/3.1">Paper</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Gifts
+              <NavDropdown.Item href="#action/3.2">Gifts</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">
+                Engineering
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Engineering</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-               Art
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Art</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#pricing">New-Arrivals</Nav.Link>
-            <NavDropdown title="Gift Festival Season" id="collapsible-nav-dropdown">
+            <Nav.Link href="#pricing" className="pe-2">New-Arrivals</Nav.Link>
+            <NavDropdown
+              title="Gift Festival Season"
+              id="collapsible-nav-dropdown"
+              className="pe-2"
+            >
               <NavDropdown.Item href="#action/3.1">Paper</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Gifts
+              <NavDropdown.Item href="#action/3.2">Gifts</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">
+                Engineering
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Engineering</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-               Art
-              </NavDropdown.Item>
-            </NavDropdown>
-
-            <NavDropdown title="Back to School" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Paper</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Gifts
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Engineering</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-               Art
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Art</NavDropdown.Item>
             </NavDropdown>
 
-            <NavDropdown title="Beach Tools" id="collapsible-nav-dropdown">
+            <NavDropdown title="Back to School" id="collapsible-nav-dropdown" className="pe-2">
               <NavDropdown.Item href="#action/3.1">Paper</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Gifts
+              <NavDropdown.Item href="#action/3.2">Gifts</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">
+                Engineering
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Engineering</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-               Art
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Art</NavDropdown.Item>
             </NavDropdown>
-            
-            <Nav.Link href="#pricing">Promotions</Nav.Link>
-            <NavDropdown title="Brands" id="collapsible-nav-dropdown">
+
+            <NavDropdown title="Beach Tools" id="collapsible-nav-dropdown" className="pe-2">
               <NavDropdown.Item href="#action/3.1">Paper</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Gifts
+              <NavDropdown.Item href="#action/3.2">Gifts</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">
+                Engineering
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Engineering</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-               Art
+              <NavDropdown.Item href="#action/3.4">Art</NavDropdown.Item>
+            </NavDropdown>
+
+            <Nav.Link href="#pricing" className="pe-2">Promotions</Nav.Link>
+            <NavDropdown title="Brands" id="collapsible-nav-dropdown" className="pe-2">
+              <NavDropdown.Item href="#action/3.1">Paper</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Gifts</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">
+                Engineering
               </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Art</NavDropdown.Item>
             </NavDropdown>
           </Nav>
-         
         </Navbar.Collapse>
       </Container>
     </Navbar>
