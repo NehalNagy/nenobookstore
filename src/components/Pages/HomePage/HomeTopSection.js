7c8f928600e9137aaad8e87b4648db5a697bafb0
NehalNagy <nehal.nagy@gmail.com>
2023-10-25 14:17:42 +0300

    started images section of home page..progress

--- src/components/Pages/HomePage/HomeTopSection.js
+++ src/components/Pages/HomePage/HomeTopSection.js
@@ -1,7 +1,26 @@
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+
 function HomeTopSection() {
   return (
     <>
-      <h5>Images</h5>
+      <Container>
+      <Row>
+        <Col xs={6} md={4}>
+          <Image src="holder.js/171x180" rounded />
+        </Col>
+        <Col xs={6} md={4}>
+          <Image src="holder.js/171x180" roundedCircle />
+        </Col>
+        <Col xs={6} md={4}>
+          <Image src="holder.js/171x180" thumbnail />
+        </Col>
+      </Row>
+    </Container>
+
+
       <h5>Policies</h5>
     </>
   );
