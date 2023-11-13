b0021ae1451928cb7bb2b3b03ff3c49e68e35de5
nehal nagy <36546667+NehalNagy@users.noreply.github.com>
2023-11-13 11:45:06 +0000

    started login/signup form

--- src/components/account/Authentication.js
+++ src/components/account/Authentication.js
@@ -1,17 +1,18 @@
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
-import Image from "react-bootstrap/Image";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
-import loginImage from "../../images/login-image.jpg";
-import { Form } from "react-router-dom";
 import classes from "./Authentication.module.css";
+import logo from "../../images/Logo_S_A.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faCartShopping } from "@fortawesome/free-solid-svg-icons";
 function Authentication() {
   return (
     <Container fluid>
       <Row>
         <Col xs={12} md={6} className={classes.imageContainer + " p-0"}>
-          <Image src={loginImage} className={classes.loginBanner} />
+          {/* <Image src={loginImage} className={classes.loginBanner} /> */}
+          <div className={classes.loginImage}></div>
           <div className={classes.imageText}>
             <p>You have an account?</p>
             <h3 className="mb-3">Log in to your account</h3>
@@ -20,8 +21,29 @@ function Authentication() {
             </Button>
           </div>
         </Col>
-        <Col xs={12} md={6} className="p-0">
-          <Form></Form>
+        <Col xs={12} md={6}>
+          <div className={classes.loginWarapper}>
+            <div className={classes.logo}>
+              <img src={logo} alt="website logo" />
+            </div>
+            <h3>Create an Account</h3>
+            <p> Create an account with</p>
+            <Button
+              type="submit"
+              variant="dark"
+              className={classes.addToCartBtn}
+            >
+              <FontAwesomeIcon icon={faCartShopping} /> Google
+            </Button>
+            <Button
+              type="submit"
+              variant="dark"
+              className={classes.addToCartBtn}
+            >
+              <FontAwesomeIcon icon={faCartShopping} /> facebook
+            </Button>
+            <form></form>
+          </div>
         </Col>
       </Row>
     </Container>
