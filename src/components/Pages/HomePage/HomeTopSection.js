9a7c277aa689a049baa97c2e40530f1bbcb0aa43
nehal nagy <36546667+NehalNagy@users.noreply.github.com>
2023-11-22 10:34:07 +0000

    align more links of policy section in home page to bottom

--- src/components/Pages/HomePage/HomeTopSection.js
+++ src/components/Pages/HomePage/HomeTopSection.js
@@ -32,30 +32,30 @@ function HomeTopSection() {
 
       <Container className={classes.policyContainer + " mt-3"}>
         <Row>
-          <Col xs={6} md={3} className={classes.rightBorder + " mb-2"}>
+          <Col xs={6} md={3} className={classes.policyItem + " mb-2"}>
             <h5>Exchange And Return Policy</h5>
-            <p>You Can Refund Your Purchases Within 30 Days</p>
-            <a title="" href="#">
+            <p className="mb-5">You Can Refund Your Purchases Within 30 Days</p>
+            <a href="/" className={classes.moreLinks}>
               More
             </a>
           </Col>
-          <Col xs={6} md={3} className={classes.rightBorder + " mb-2"}>
+          <Col xs={6} md={3} className={classes.policyItem + " mb-2"}>
             <h5>Multiple Payment Methods</h5>
-            <p>You Can Pay In More Than One Way Safely</p>
-            <a title="" href="#">
+            <p className="mb-5">You Can Pay In More Than One Way Safely</p>
+            <a href="/" className={classes.moreLinks}>
               More
             </a>
           </Col>
-          <Col xs={6} md={3} className={classes.rightBorder + " mb-2"}>
+          <Col xs={6} md={3} className={classes.policyItem + " mb-2"}>
             <h5>Shipping And Delivery Policy</h5>
-            <p>Shipping To All Provinces</p>
-            <a title="" href="#">
+            <p className="mb-5">Shipping To All Provinces</p>
+            <a href="/" className={classes.moreLinks}>
               More
             </a>
           </Col>
-          <Col xs={6} md={3} className={classes.rightBorder + " mb-2"}>
-            <h5>Support All Days Of The Week</h5>
-            <a title="" href="#">
+          <Col xs={6} md={3} className={classes.policyItem + " mb-2"}>
+            <h5 className="mb-5">Support All Days Of The Week</h5>
+            <a href="/" className={classes.moreLinks}>
               More
             </a>
           </Col>
