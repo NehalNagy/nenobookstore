e1a95e6de0842b105b4171444c4d513c50f2e2d4
nehal nagy <36546667+NehalNagy@users.noreply.github.com>
2023-12-11 09:55:12 +0000

    added swiper  to itemslist

--- src/components/Pages/ItemsList.js
+++ src/components/Pages/ItemsList.js
@@ -1,9 +1,49 @@
 import ItemCard from "./HomePage/ItemCard";
-import classes from "./pages.module.css";
+import Image from "react-bootstrap/Image";
+import SubCategory from "../../images/bd.jpg";
+import generalClasses from "./pages.module.css";
+import classes from "./ItemsList.module.css";
+import { register } from "swiper/element/bundle";
+import { useRef, useEffect } from "react";
+
+register();
+
 function ItemsList() {
+  const swiperElRef = useRef(null);
+
   return (
-    <div className={classes.pageContentWrapper + " container"}>
-      <div className="mb-5">carousel</div>
+    <div className={generalClasses.pageContentWrapper + " container"}>
+      <div className="m-5">
+        <swiper-container
+          ref={swiperElRef}
+          slides-per-view="3"
+          navigation="true"
+          pagination="true"
+          autoplay-delay="5000"
+          loop="true"
+          autoplay-disable-on-interaction="false"
+          autoplay-reverse-direction="true"
+        >
+          <swiper-slide>
+            <Image src={SubCategory} className={classes.swiperImages} />
+          </swiper-slide>
+          <swiper-slide>
+            <Image src={SubCategory} className={classes.swiperImages} />
+          </swiper-slide>
+          <swiper-slide>
+            <Image src={SubCategory} className={classes.swiperImages} />
+          </swiper-slide>
+          <swiper-slide>
+            <Image src={SubCategory} className={classes.swiperImages} />
+          </swiper-slide>
+          <swiper-slide>
+            <Image src={SubCategory} className={classes.swiperImages} />
+          </swiper-slide>
+          <swiper-slide>
+            <Image src={SubCategory} className={classes.swiperImages} />
+          </swiper-slide>
+        </swiper-container>
+      </div>
       <div className="row">
         <div className="col-md-2">filter</div>
         <div className="col-md-10">
