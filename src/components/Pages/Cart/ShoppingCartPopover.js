f38c04a5988a9f1df988bae70d8066fef1aa43f5
NehalNagy <nehal.nagy@gmail.com>
2023-12-13 13:25:21 +0200

    added buttons to cart popover

--- src/components/Pages/Cart/ShoppingCartPopover.js
+++ src/components/Pages/Cart/ShoppingCartPopover.js
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
 import CartItem from "../../../images/backpack.jpg";
 import classes from "./ShoppingCartPopover.module.css";
+import { Button } from "react-bootstrap";
 
 function ShoppingCartPopover() {
   return (
@@ -13,7 +14,9 @@ function ShoppingCartPopover() {
         </div>
         <div className="col-md-5">
           <span className={classes.cartItemCode}>6111501131100</span>
-          <p>MUST KINDERGARTEN BACKPACK LITTLE FAIRY <br/> No: 84489</p>
+          <p>
+            MUST KINDERGARTEN BACKPACK LITTLE FAIRY <br /> No: 84489
+          </p>
         </div>
         <div className="col-md-2">x 1</div>
         <div className="col-md-2">1500 LE</div>
@@ -21,12 +24,26 @@ function ShoppingCartPopover() {
           <FontAwesomeIcon icon={faTrashCan} />
         </div>
       </div>
-      <div className="row">
+      <div className="row mt-5">
         <div className="col-6">
           <h5>Cart Subtotal</h5>
         </div>
-        <div className="col-6 text-end"><h5>1500 LE</h5></div>
+        <div className="col-6 text-end">
+          <h5>1500 LE</h5>
+        </div>
+
+      
       </div>
+      <div className="row mt-3">  <div className="col-6">
+          <Button type="submit" variant="dark" className={classes.cartPopoverBtn}>
+             Ckeckout
+          </Button>
+        </div>
+        <div className="col-6">
+          <Button type="submit" variant="outline-dark" className={classes.cartPopoverBtn}>
+             Edit cart
+          </Button>
+        </div></div>
     </div>
   );
 }
