106aa4674a9349a226e486e02e108ad101ba1df7
nehal nagy <36546667+NehalNagy@users.noreply.github.com>
2023-12-25 11:11:33 +0000

    complete  checkout section in cart component

--- src/components/Pages/Cart/Cart.js
+++ src/components/Pages/Cart/Cart.js
@@ -1,6 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { Button } from "react-bootstrap";
 import CartItem from "./CartItem";
 import classes from "./Cart.module.css";
 import CartItemQuantityBtn from "./CartItemQuantityBtn";
@@ -46,7 +47,7 @@ function Cart() {
             </tbody>
           </table>
         </Col>
-        <Col md={4} className={classes.checkoutPanel}>
+        <Col md={4} className={classes.checkoutPanel + " ps-3 pe-3"}>
           <h3 className="pt-3 pb-3">Shopping Cart Total</h3>
           <Row>
             <Col md={6}>
@@ -56,7 +57,7 @@ function Cart() {
               <h5>3,394.00ج.م</h5>
             </Col>
           </Row>
-          <Row>
+          <Row className={classes.horizontalLine}>
             <Col md={6}>
               <h5>Tax</h5>
             </Col>
@@ -64,6 +65,33 @@ function Cart() {
               <h5>0.00ج.م</h5>
             </Col>
           </Row>
+          <Row className={classes.horizontalLine + " mt-4 mb-4"}>
+            <Col md={6}>
+              <h4>Order Total</h4>
+            </Col>
+            <Col md={6} className="text-end">
+              <h5>3,394.00ج.م</h5>
+            </Col>
+          </Row>
+
+          <h4>Apply Discount Code</h4>
+          <input
+            type="text"
+            placeholder="Enter discount code"
+            className={classes.discountCodeInput}
+          />
+          <Button type="button" variant="dark" className={classes.discountBtn}>
+            Apply Discount
+          </Button>
+          <div className={classes.checkoutBtnWrapper}>
+            <Button
+              type="button"
+              variant="dark"
+              className={classes.checkoutBtn}
+            >
+              Proceed to Checkout
+            </Button>
+          </div>
         </Col>
       </Row>
     </Container>
